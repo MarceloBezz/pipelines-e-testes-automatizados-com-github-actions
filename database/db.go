dc3fa257811c0799cf3788373fedc40587b5a10c
Marcelo <mdbezerra16@gmail.com>
2025-12-10 11:54:47 -0300

    Alterações para pull request

--- database/db.go
+++ database/db.go
@@ -14,12 +14,12 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbPort := os.Getenv("DB_PORT")
-	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable"
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
+	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
