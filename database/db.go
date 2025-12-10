a72b87da51a8484c654c06640cfd484419bee369
Marcelo <mdbezerra16@gmail.com>
2025-12-10 11:37:28 -0300

    Refatoração de código

--- database/db.go
+++ database/db.go
@@ -22,7 +22,7 @@ func ConectaComBancoDeDados() {
 	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados\n" + err)
+		log.Panic("Erro ao conectar com banco de dados")
 	}
 
 	_ = DB.AutoMigrate(&models.Aluno{})
